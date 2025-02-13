49f10bde8175c2c292e515d5272f4e52602e08d8
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-02-13 11:47:13 -0700

    Convert about to React, format jsx, delete html

--- src/about/about.jsx
+++ src/about/about.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import './about.css';
 
 export function About() {
     return (
-      <main className="container-fluid bg-secondary text-center">
-        <div>about displayed here</div>
-      </main>
+      <main>
+            <div className = "chat-box">
+                <div className = "text">
+                    <p>DraftMagic is a online platform to draft Magic The Gathering in pods, and then be able to export the resulting decklist.</p>
+                    <p>Scryfall API is used to provide images of the cards, and to get the card information.</p>
+                </div>
+            </div>
+        </main>
     );
   }
