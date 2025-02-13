4d6544fd76ea6f0fd7fa52e4049df7b5c9a664be
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-02-13 12:28:51 -0700

    Convert draft to React

--- src/draft/draft.jsx
+++ src/draft/draft.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
+import './draft.css';
+import { Decklist } from '../decklist/decklist';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 export function Draft() {
     return (
-      <main className="container-fluid bg-secondary text-center">
-        <div>draft displayed here</div>
-      </main>
+      <main>
+            <h1>
+                Pack 1 of 3
+            </h1>
+            <div className = "container">
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+                <img alt = "Card" src = "./Card back.webp" width = "275" height = "400" />
+            </div>
+            <br />
+            <br />
+            <NavLink className = "nav-link" to = "/decklist">Save</NavLink>
+        </main>
     );
   }
