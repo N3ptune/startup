d8f8ab43e9deb4385d77bb0a1a145e021146f847
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-02-13 12:33:09 -0700

    Convert login to react, somehow fixed the other pages

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
+import './index.css';
+import { Lobby } from '../lobby/lobby';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
-    </main>
+    <main>
+            <h1>
+                Welcome to DraftMagic
+            </h1>
+            <form method = "get" action = "lobby.html">
+                <div>
+                    <h4>
+                        Username
+                    </h4>
+                    <input type = "text" placeholder="Email or Username" />
+                </div>
+                <div>
+                    <h4>
+                        Password
+                    </h4>
+                    <input type = "password" placeholder="Password" />
+                </div>
+                <NavLink className = "nav-link" to = "/lobby">Login</NavLink>
+                <NavLink className = "nav-link" to = "/lobby">Create</NavLink>
+            </form>
+            <hr />
+        </main>
   );
 }
