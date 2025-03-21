f86b96710f8f3d1c22eeb74554a2ee42981810b2
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-03-20 22:33:20 -0600

    Make authenticated, but I don't think it's needed

---
+++ src/login/authenticated.jsx
@@ -0,0 +1,27 @@
+// I DON'T THINK I NEED THIS
+// LOGIN WILL GO TO LOBBY
+// LOGOUT BUTTON WILL BE ONHIBDE IN HEADER
+
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import Button from 'react-bootstrap/Button';
+
+import './authenticated.css';
+
+export function authenticated(props) {
+    const navigate = useNavigate();
+
+    function logout(){
+        fetch('/api/auth/logout', {
+            method: 'DELETE',
+        })
+        .catch(() => {
+            // Logout failed
+        })
+        .finally(() => {
+            localStorage.removeItem('userName');
+            props.onLogout();
+        });
+    }
+}
