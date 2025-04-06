28fac52289c3748c67afab81a1ca340e87f19fd8
N3ptune <jasonjbrooks2014@gmail.com>
2025-04-06 10:27:51 -0600

    implement db

--- service/index.js
+++ service/index.js
@@ -3,6 +3,7 @@ const bcrypt = require('bcryptjs');
 const express = require('express');
 const uuid = require('uuid');
 const app = express();
+const DB = require('./database.js');
 
 const authCookieName = 'token';
 
@@ -44,6 +45,7 @@ apiRouter.post('/auth/login', async (req, res) => {
     if (user) {
         if (await bcrypt.compare(req.body.password, user.password)) {
             user.token = uuid.v4();
+            await DB.updateUser(user);
             setAuthCookie(res, user.token);
             res.send({email: user.email});
             return;
@@ -53,10 +55,11 @@ apiRouter.post('/auth/login', async (req, res) => {
 });
 
 // Logout a user
-apiRouter.post('/user/logout', async (req, res) => {
+apiRouter.delete('/auth/logout', async (req, res) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
         delete user.token;
+        DB.updateUser(user);
     }
     res.clearCookie(authCookieName);
     res.status(204).end();
@@ -79,7 +82,7 @@ async function createUser(email, password){
         password: passwordHash,
         token: uuid.v4(),
     };
-    users.push(user);
+    await DB.addUser(user);
     return user;
 }
 
@@ -87,7 +90,10 @@ async function findUser(field, value){
     if (!value) {
         return null;
     }
-    return users.find((u) => u[field] === value);
+    if (field === 'token') {
+        return DB.getUserByToken(value);
+      }
+      return DB.getUser(value);
 }
 
 function setAuthCookie(res, authToken){
