327e6e45f634b4380ceedfb41ffbb8ea8af08eaa
Jason Brooks <jasonjbrooks2014@gmail.com>
2025-04-08 09:38:08 -0600

    Change api path for logout

--- service/index.js
+++ service/index.js
@@ -59,7 +59,7 @@ apiRouter.post('/auth/login', async (req, res) => {
 });
 
 // Logout a user
-apiRouter.delete('/auth/logout', async (req, res) => {
+apiRouter.delete('/auth/delete', async (req, res) => {
     const user = await findUser('token', req.cookies[authCookieName]);
     if (user) {
         delete user.token;
